05e7d7b5f2014d65b4a62b3615d1fd1c58d8d51c
Dennis <dbunadi@gmail.com>
2019-07-24 12:53:17 +1000

    added entity data and pos tag mapping

--- components.py
+++ components.py
@@ -5,11 +5,27 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
+gcp_to_nltk_pos_tags = {
+    enums.PartOfSpeech.Tag.ADJ: 'JJ',
+    enums.PartOfSpeech.Tag.ADP: 'IN',
+    enums.PartOfSpeech.Tag.ADV: 'RB',
+    enums.PartOfSpeech.Tag.CONJ: 'CC',
+    enums.PartOfSpeech.Tag.DET: 'DT',
+    enums.PartOfSpeech.Tag.NOUN: 'NN',
+    enums.PartOfSpeech.Tag.NUM: 'CD',
+    enums.PartOfSpeech.Tag.PRON: 'PRP',
+    enums.PartOfSpeech.Tag.PRT: 'RP',
+    enums.PartOfSpeech.Tag.PUNCT: '.',
+    enums.PartOfSpeech.Tag.VERB: 'VB',
+    enums.PartOfSpeech.Tag.X: '.',
+    enums.PartOfSpeech.Tag.AFFIX: '.'
+}
+
+
 # TODO: separate the parsing stuff
-def parse_lo(lo):
+def syntax_chunk(tokenised):
 
     result = {}
-    tokenised = nltk.word_tokenize(lo)
     pattern = """
         V: {<VB>|<VDB>}
         P: 
@@ -20,7 +36,7 @@ def parse_lo(lo):
     parser = nltk.RegexpParser(pattern)
     st = nltk.tag.stanford.StanfordPOSTagger('./stanford-postagger/models/english-bidirectional-distsim.tagger', './stanford-postagger/stanford-postagger.jar')
     
-    result['tree'] = parser.parse(st.tag(tokenised))
+    result['tree'] = parser.parse(tokenised)
 
     result['phrases'] = []
     # check one level below root
@@ -34,39 +50,45 @@ def parse_lo(lo):
             result['verb'].append(item)
 
     return result
-
- # TODO: score verbs on where they land in a dependency parse tree. they should be the root
-  
+   
 def basic_stats(lo):
     #
     return 1
 
+# TODO: rename it away from syntax_analysis
 def syntax_analysis(lo):
     # Call the natural language api
     client = language.LanguageServiceClient()
     document = types.Document(content=lo, type=enums.Document.Type.PLAIN_TEXT);
-    raw_result = client.analyzeSyntax(document)
+    s_result = client.analyze_syntax(document)
+    e_result = client.analyze_entities(document)
 
     result = {}
     # TODO: map gcp pos enum to a standard
-    result['pos'] = [(word.text.content, word.part_of_speech.tag) for word in raw_result.tokens]
+    result['pos'] = [(word.text.content, gcp_to_nltk_pos_tags[word.part_of_speech.tag]) for word in s_result.tokens]
+    
     # TODO: make dependency tree
-    result['d_tree'] = ''
+    result['d_tree'] = [{'text': word.text.content, 'd_label': word.dependency_edge.label, 'pos': word.part_of_speech.tag} for word in s_result.tokens]
+
     result['sentences'] = {
-        'count': len(raw_result.sentences),
-        'sentences': [s.text.content for s in raw_result.sentences]
+        'count': len(s_result.sentences),
+        'sentences': [s.text.content for s in s_result.sentences]
     }
 
+    result['entities'] = {}
+    for item in e_result.entities:
+        result['entities'][item.name] = {
+            'type': item.type,
+            'salience': item.salience, 
+        }   
 
-def is_verb_root(d_tree):
-    # TODO: Check if the verb is close to the root of the dependency tree
+    return result
 
-    return True
+def is_root_verb(d_tree):
+    return True if list(filter(lambda x: x['d_label'] == enums.DependencyEdge.Label.ROOT, d_tree))[0]['pos'] == enums.PartOfSpeech.Tag.VERB else False
 
-def get_bt_category(verb):
+#def get_bt_category(verb):
     # TODO: fetch bt category by checking sysnonyms of the verb
     # get synset for verb based of lesk in context with the whole lo
     # for each category, path_similarity the verb to the category
     # return match with the highest score
-
-
